a968a9e593ca1bc930007c81c8cf790120542567
xiaoxuyou <18740459067@163.com>
2019-09-10 18:40:21 +0800

    第一次修改

---
+++ git/git/src/cn/itcast/demo/git.java
@@ -0,0 +1,10 @@
+package cn.itcast.demo;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+    public void test(){
+        System.out.println("修改一次");
+    }
+}
